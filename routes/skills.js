139133c09a350e6d56bcaea5ed3241e806bcdced
sumardey5 <sumardey5@gmail.com>
2020-02-26 09:35:19 -0600

    create routes in routes/skills.js

--- routes/skills.js
+++ routes/skills.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
 const skillsCtrl = require('../controllers/skills');
 
 /* GET users listing. */
@@ -12,8 +11,12 @@ const skillsCtrl = require('../controllers/skills');
 
 //Index route
 router.get('/', skillsCtrl.index);
-
+router.get('/new', skillsCtrl.new);
 //Show route
 router.get('/:id', skillsCtrl.show);
+router.post('/', skillsCtrl.create);
+router.delete('/:id', skillsCtrl.delete);
+router.get('/:id/edit', skillsCtrl.edit);
+router.put('/:id', skillsCtrl.update);
 
 module.exports = router;
